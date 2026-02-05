55858f70cbf3e507f4203121754d7ab43614e258
Priyanshu Saxena <priyanshusaxena221@gmail.com>
2026-02-05 11:00:34 +0530

    similiar question to Q12

--- objectsAndArr/Q12.js
+++ objectsAndArr/Q12.js
@@ -20,3 +20,27 @@ console.log(result.join(" > "));
 
 //learned abt sorting questions today 
 //got logic from gpt
+
+//another Question
+
+const marks = { 
+    ram: 45, 
+    shyam: 12, 
+    mohan: 67, 
+    amit: 30 
+};
+
+let result2 = [];
+
+let entries2 = Object.entries(marks);
+
+//sort by value
+
+entries2.sort((x,y)=> x[1]-y[1]);
+
+for(let [key, value] of entries2){
+    result2.push(`${key}: ${value}`)
+}
+
+console.log(result2.join(" > "));
+
